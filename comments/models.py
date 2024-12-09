734d98254a82a9b2c799a47f84b113edb6199968
Danyil Artomov <dartomov@gmail.com>
2024-12-09 08:48:03 +0100

    Implement comments model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from dummy_user.models import DummyUserModel
+
+
+class CommentsModel(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(DummyUserModel, on_delete=models.DO_NOTHING)
+    home_page = models.URLField(null=True, blank=True)
+    response_to = models.OneToOneField("CommentsModel", on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=("id", "response_to"), name="unique_id_response_to"
+            ),
+        )
+
+    def __str__(self):
+        return f"{self.user.username} messaged: {self.text}"
