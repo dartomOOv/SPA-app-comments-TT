32fd381686794b3391c99f9388eb046ce95a942a
Danyil Artomov <dartomov@gmail.com>
2024-12-09 08:46:29 +0100

    Implement dummy user model

--- dummy_user/models.py
+++ dummy_user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class DummyUserModel(models.Model):
+    username = models.CharField(max_length=64)
+    email = models.EmailField(max_length=128)
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=("username", "email"), name="unique_username_email"
+            ),
+        )
+
+    def __str__(self):
+        return self.username
