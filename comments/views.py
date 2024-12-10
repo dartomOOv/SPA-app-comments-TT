30ffae93cc04d5bd3b0ef461ae9992874c417ebc
Danyil Artomov <dartomov@gmail.com>
2024-12-10 14:00:09 +0100

    redefine 'get_serializer_class' in comments viewset to work with comments retrieve serializer

--- comments/views.py
+++ comments/views.py
@@ -2,16 +2,16 @@ from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
 from comments.models import CommentsModel
-from comments.serializers import CommentsSerializer
+from comments.serializers import CommentsSerializer, CommentRetrieveSerializer
 
 
 class CommentsViewSet(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
     GenericViewSet
 ):
     queryset = CommentsModel.objects.all()
-    serializer_class = CommentsSerializer
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -20,3 +20,9 @@ class CommentsViewSet(
             queryset =  queryset.filter(response_to__isnull=True)
 
         return queryset
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return CommentRetrieveSerializer
+
+        return CommentsSerializer
