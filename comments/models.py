046a935cd46a96beb969009063b793b69ed3a926
Danyil Artomov <dartomov@gmail.com>
2024-12-10 14:02:59 +0100

    add posted_at datetime field for comments model and add ordering by datetime

--- comments/models.py
+++ comments/models.py
@@ -4,6 +4,7 @@ from dummy_user.models import DummyUserModel
 
 
 class CommentsModel(models.Model):
+    posted_at = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
     user = models.ForeignKey(DummyUserModel, on_delete=models.DO_NOTHING)
     home_page = models.URLField(null=True, blank=True)
@@ -15,6 +16,7 @@ class CommentsModel(models.Model):
                 fields=("id", "response_to"), name="unique_id_response_to"
             ),
         )
+        ordering = ("-posted_at",)
 
     def __str__(self):
         return f"{self.user.username} messaged: {self.text}"
