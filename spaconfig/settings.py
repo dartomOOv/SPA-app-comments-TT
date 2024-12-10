17257027b370c17cc2e46d43f7ac72ca5ccc638c
Danyil Artomov <dartomov@gmail.com>
2024-12-10 12:14:32 +0100

    add drf to project installed apps

--- spaconfig/settings.py
+++ spaconfig/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    # 3rd apps
+    "rest_framework",
+    
     # django apps
     "comments",
     "dummy_user",
