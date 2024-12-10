fd0351be6ca034580cf40cce185e9f4fc51ff79e
Danyil Artomov <dartomov@gmail.com>
2024-12-10 12:19:08 +0100

    create viewset with list and create actions for comments model

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from comments.models import CommentsModel
+from comments.serializers import CommentsSerializer
+
+
+class CommentsViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet
+):
+    queryset = CommentsModel.objects.all()
+    serializer_class = CommentsSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if self.action == "list":
+            queryset =  queryset.filter(response_to__isnull=True)
+
+        return queryset
